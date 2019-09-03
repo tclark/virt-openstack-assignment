a490e5d5a936303b59e8783ebfaa529179e2aeff
Tom Clark <tom@runawayplay.com>
2019-09-04 11:13:04 +1200

    added assignment.py file

---
+++ assignment.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import openstack
+
+def create():
+    print('performing create')
+
+def run():
+    print('performing run')
+
+def stop():
+    print('performing stop')
+
+def destroy():
+    print('performing destroy')
+
+def status():
+    print('performing status')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('operation', 
+            help='One of "create", "run", "stop", "destroy", or "status"')
+    args = parser.parse_args()
+    operation = args.operation
+
+    operations = {
+            'create'  : create,
+            'run'     : run,
+            'stop'    : stop,
+            'destroy' : destroy,
+            'status'  : status
+            }
+
+    action = operations.get(operation, lambda: print('{}: no such operation'.format(operation)))
+    action()
+
