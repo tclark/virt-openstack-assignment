c300421f296e4b017ebde889fab86fb1f7a5a215
Matthew Hall <hallmg1@student.op.ac.nz>
2019-09-07 05:15:03 +0000

    Servers can be created

--- assignment.py
+++ assignment.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python
 import argparse
 import openstack
 
+
+IMAGE          =  'ubuntu-minimal-16.04-x86_64'
+FLAVOR         =  'c1.c1r1'
+KEYPAIR        =  'hallmg1-key'
+NETWORK        =  'hallmg1-net'
+ROUTER         =  'hallmg1-router'
+SECURITY_GROUP = ['assignment2']
+SERVER_NAMES   = ['hallmg1-web',
+                 'hallmg1-app',
+                 'hallmg1-db']
+
+conn = openstack.connect(cloud_name='openstack')
+
+
 def create():
     ''' Create a set of Openstack resources '''
-    pass
+    image   = conn.compute.find_image(IMAGE)
+    flavor  = conn.compute.find_flavor(FLAVOR)
+    keypair = conn.compute.find_keypair(KEYPAIR)
+
+    # TODO: check if resources already exist before creation
+    # TODO: add network and security groups to server creation
+    for name in SERVER_NAMES:
+        conn.compute.create_server(name=name, image_id=image.id,
+                                   flavor_id=flavor.id,
+                                   key_name=keypair.name)
 
 def run():
     ''' Start  a set of Openstack virtual machines
