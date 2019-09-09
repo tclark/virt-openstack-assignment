2c8d670923885e4fc5bc09402083a3504cc4cc8d
Aleksandr Gordeev <razamanaza@yandex.ru>
2019-09-09 08:44:46 +0000

    Finish create. Still has problems with destroy

--- assignment.py
+++ assignment.py
@@ -7,19 +7,54 @@ KEYPAIR = 'mariadb'
 NETWORK = 'gorda5-net'
 SUBNET = 'gorda5-sub'
 ROUTER = 'gorda5-rtr'
-SERVER1 = 'gorda5-web'
-SERVER2 = 'gorda5-app'
-SERVER3 = 'gorda5-db'
+IP_VERSION = 4
+CIDR = '192.168.50.0/24'
+SECURITY_GROUP = 'default'
+SERVERNAMES = ['gorda5-web', 'gorda5-app', 'gorda5-db']
 
 conn = openstack.connect(cloud_name='openstack')
 
+net = conn.network.find_network(NETWORK)
+rtr = conn.network.find_router(ROUTER)
+sub = conn.network.find_subnet(SUBNET)
+public_net = conn.network.find_network('public-net')
+
 def create():
     ''' Create a set of Openstack resources '''
-    if not conn.network.find_network(NETWORK):
-        network = conn.network.create_network(name=NETWORK)
-        subnet = conn.network.create_subnet(name=SUBNET, cidr='192.168.50.0/24', network_id=network.id, ip_version='4')
+    image = conn.compute.find_image(IMAGE)
+    flavor = conn.compute.find_flavor(FLAVOUR)
+    keypair = conn.compute.find_keypair(KEYPAIR)
+    security_group = conn.network.find_security_group(SECURITY_GROUP)
+
+    if not net:
+        net_new = conn.network.create_network(name=NETWORK)
+        sub_new = conn.network.create_subnet(name=SUBNET, cidr=CIDR, network_id=net_new.id, ip_version=IP_VERSION)
     else:
-        print('Network already exist')
+        print(f'Network {NETWORK} already exists')
+
+    if not rtr:
+        rtr_new = conn.network.create_router(name=ROUTER, external_gateway_info={'network_id': public_net.id})
+        conn.network.add_interface_to_router(rtr_new, sub_new.id)
+    else:
+        print(f'Router {ROUTER} already exists')
+
+    for servername in SERVERNAMES:
+        if not conn.compute.find_server(servername):
+            server = conn.compute.create_server(
+                name=servername,
+                image_id=image.id, 
+                flavor_id=flavor.id, 
+                networks=[{'uuid':net_new.id}], 
+                key_name=keypair.name, 
+                security_groups=[{'sgid':security_group.id}]
+            )
+            if servername == 'gorda5-web':
+                conn.compute.wait_for_server(server)
+                fip = conn.network.create_ip(floating_network_id=public_net.id)
+                conn.compute.add_floating_ip_to_server(server, fip.floating_ip_address)
+                print(f"Web server has address: {fip.floating_ip_address}")
+        else:
+            print(f"Server {servername} already exists")
 
 def run():
     ''' Start  a set of Openstack virtual machines
@@ -37,9 +72,21 @@ def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
-    if conn.network.find_network(NETWORK):
-        network = conn.network.find_network(NETWORK)
-        conn.network.delete_network(network)
+    for servername in SERVERNAMES:
+        server = conn.compute.find_server(servername)
+        if server:
+            conn.compute.delete_server(server)
+    
+    if rtr:
+        conn.network.remove_interface_from_router(rtr, sub.id)
+        conn.network.delete_router(rtr)
+    
+    if net:
+        for s in net.subnet_ids:
+            conn.network.delete_subnet(s)
+        conn.network.delete_network(net)
+
+
 
 def status():
     ''' Print a status report on the OpenStack
