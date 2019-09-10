4938f0aefd9578f2bb3a7ebd2d9895b5aed28d45
Matthew Hall <hallmg1@student.op.ac.nz>
2019-09-10 03:30:00 +0000

    All resources can be created missing some configuration

--- assignment.py
+++ assignment.py
@@ -3,31 +3,52 @@ import argparse
 import openstack
 
 
-IMAGE          =  'ubuntu-minimal-16.04-x86_64'
-FLAVOR         =  'c1.c1r1'
-KEYPAIR        =  'hallmg1-key'
-NETWORK        =  'hallmg1-net'
-ROUTER         =  'hallmg1-router'
-SECURITY_GROUP = ['assignment2']
-SERVER_NAMES   = ['hallmg1-web',
-                 'hallmg1-app',
-                 'hallmg1-db']
+IMAGE = 'ubuntu-minimal-16.04-x86_64'
+FLAVOR = 'c1.c1r1'
+KEYPAIR = 'hallmg1-key'
+SUBNET = 'hallmg1-subnet'
+NETWORK = 'hallmg1-net'
+ROUTER = 'hallmg1-router'
+SECURITY_GROUP = 'assignment2'
+SERVER_NAMES = ['hallmg1-web', 'hallmg1-app', 'hallmg1-db']
 
 conn = openstack.connect(cloud_name='openstack')
 
 
 def create():
     ''' Create a set of Openstack resources '''
-    image   = conn.compute.find_image(IMAGE)
-    flavor  = conn.compute.find_flavor(FLAVOR)
+    image = conn.compute.find_image(IMAGE)
+    flavor = conn.compute.find_flavor(FLAVOR)
     keypair = conn.compute.find_keypair(KEYPAIR)
 
     # TODO: check if resources already exist before creation
-    # TODO: add network and security groups to server creation
+    # TODO: floating ip
+    # TODO: router interfaces
+
+    print('Creating', NETWORK)
+    network = conn.network.create_network(name=NETWORK)
+    print('Creating', SUBNET)
+    subnet = conn.network.create_subnet(name=SUBNET, network_id=network.id,
+        cidr='192.168.50.0/24', ip_version=4)
+
+    print('Creating', ROUTER)
+    router = conn.network.create_router(name=ROUTER)
+
+    security_group = conn.network.find_security_group(SECURITY_GROUP)
+
+    #public_net = conn.network.find_network('public-net')
+    #web_ip = conn.network.create_ip(floating_network_id=public_net.id)
+
     for name in SERVER_NAMES:
-        conn.compute.create_server(name=name, image_id=image.id,
-                                   flavor_id=flavor.id,
-                                   key_name=keypair.name)
+        print('Creating', name)
+        s = conn.compute.create_server(name=name, image_id=image.id,
+            flavor_id=flavor.id, key_name=keypair.name,
+            networks=[{'uuid': network.id}],
+            security_groups=[security_group])
+        # TODO: move this out of loop when checking is implemented
+        #if name == 'hallmg1-web':
+            #conn.compute.add_floating_ip_to_server(s, web_ip.floating_ip_address)
+
 
 def run():
     ''' Start  a set of Openstack virtual machines
@@ -35,17 +56,20 @@ def run():
     '''
     pass
 
+
 def stop():
     ''' Stop  a set of Openstack virtual machines
     if they are running.
     '''
     pass
 
+
 def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
     pass
+
 
 def status():
     ''' Print a status report on the OpenStack
