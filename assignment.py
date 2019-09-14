3fcc3a3ee4000a87625cd564555d42a1c56114f5
Hua Wang <wangh21@student.op.ac.nz>
2019-09-14 09:06:53 +0000

    update create

--- assignment.py
+++ assignment.py
@@ -1,9 +1,35 @@
 import argparse
 import openstack
 
+IMAGE = 'ubuntu-minimal-16.04-x86_64'
+FLAVOUR = 'c1.c1r1'
+SERVERLIST = ['wangh21-web','wangh21-app','wangh21-db']
+KEYPAIRNAME= 'hua'
+NETWORK = 'wangh21-net'
+SUBNET = '192.168.50.0/24'
+ROUTER = 'wangh21-rtr'
+
+conn = openstack.connect(cloud_name=’openstack’)
+
+
+
 def create():
     ''' Create a set of Openstack resources '''
-    pass
+    conn.netowrk.find_network(NETWORK)==None? network = conn.network.create_network(name=NETWORK) : print(f'Network {NETWORK} already exists...')
+
+    for server in SERVERLIST:
+        is_exists = conn.compute.find_server(server)
+        if(is_exists):
+            print(f'The Server {server} already exists...')
+        else:
+            print(f'Start create {server}, please wait...')
+            conn.compute.create_server(
+                    name=server,
+                    image_id=image.id,
+                    flavor_id=flavor.id,
+                    networks=[{"uuid": network}],
+                    key_name='hua'
+                    )
 
 def run():
     ''' Start  a set of Openstack virtual machines
