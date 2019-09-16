dd4cc9ad3ff325d7157bd02ece6766915602f6e1
zimbardoex <zimbardoex@gmail.com>
2019-09-16 13:40:10 +1200

    Move create actions to separate script

--- assignment.py
+++ assignment.py
@@ -1,88 +1,23 @@
 #!/usr/bin/env
 import argparse
-import openstack
+import utilities
 
-KEYPAIR = 'sysadminapp'
-conn = openstack.connect(could_name='openstack')
-
-SUBNET_NAME = 'chril2-subnet'
-SUBNET_IP_VERSION = 4
-SUBNET_CIDR = '192.168.50.0/24'
 NETWORK_NAME = 'chril2-net'
+SUBNET_NAME = 'chril2-subnet'
 PUBLIC_NETWORK_NAME = 'public-net'
-
 ROUTER_NAME = 'chril2-rtl'
-SERVERS = ['chril2-web', 'chril2-app', 'chril2-db']
-IMAGE = 'ubuntu-minimal-16.04-x86_64'
-FLAVOUR = 'c1.c1r1'
-SECURITY_GROUP = 'default'
+SERVER_NAMES = ['chril2-web', 'chril2-app', 'chril2-db']
 
 def create():
     ''' Create a set of Openstack resources '''
-    network = conn.network.find_network(NETWORK_NAME)
-    if(network == None):
-        print(f'\nCreating network {NETWORK_NAME}...')
-        network = conn.network.create_network(name=NETWORK_NAME)
-    else:
-        print(f'\nNetwork {NETWORK_NAME} already exists - skipping')
-
-    subnet = conn.network.find_subnet(SUBNET_NAME)
-    if(subnet == None):
-        print(f'\nCreating subnet {SUBNET_NAME}...')
-        subnet = conn.network.create_subnet(name=SUBNET_NAME, network_id=network.id, ip_version=SUBNET_IP_VERSION, cidr=SUBNET_CIDR)
-    else:
-        print(f'\nSubnet {SUBNET_NAME} already exists - skipping')
-
-    public_net = conn.network.find_network(PUBLIC_NETWORK_NAME)
-    if(public_net == None):
-        print(f'\nCOULD NOT FIND NETWORK {PUBLIC_NETWORK_NAME}')
-
-    router = conn.network.find_router(ROUTER_NAME)
-    if (router == None):
-        print(f'\nCreating router {ROUTER_NAME}...')
-        router = conn.network.create_router(name=ROUTER_NAME, external_gateway_info={'network_id': public_net.id})
-        router = conn.network.add_interface_to_router(router, subnet.id)
-    else:
-        print(f'\nRouter {ROUTER_NAME} already exists - skipping')
-
-    image = conn.compute.find_image(IMAGE)
-    if(image == None):
-        print(f'\nCOULD NOT FIND IMAGE {IMAGE}')
-
-    flavour = conn.compute.find_flavor(FLAVOUR)
-    if(flavour == None):
-        print(f'\nCOULD NOT FIND FLAVOUR {FLAVOUR}')
-    
-    keypair = conn.compute.find_keypair(KEYPAIR)
-    if(keypair == None):
-        print(f'\nCOULD NOT FIND KEYPAIR {KEYPAIR}')
-    
-    security_group = conn.network.find_security_group(SECURITY_GROUP)
-    if(security_group == None):
-        print(f'\nCOULD NOT FIND SECURITY GROUP {SECURITY_GROUP}')
-    
-    for server_name in SERVERS:
-        server = conn.compute.find_server(server_name)
-        if( server == None):
-            print(f'\nCreating server {server_name}...')
-            server = conn.compute.create_server(
-                    name=server_name, image_id=image.id, 
-                    flavor_id=flavour.id, networks=[{'uuid':network.id}], # Should all have IP addresses?
-                    key_name=keypair.name, security_groups=[{'sgid':security_group.id}]
-            )
-        else:
-            print(f'\nServer {server_name} already exists - skipping')
-        
-        # Adding floating IP to web server.
+    network = utilities.create_network(NETWORK_NAME)
+    subnet = utilities.create_subnet(SUBNET_NAME, network)
+    public_net = utilities.find_public_network(PUBLIC_NETWORK_NAME)
+    utilities.create_router(ROUTER_NAME, subnet, public_net)
+    for server_name in SERVER_NAMES:
+        utilities.create_server(server_name, network)
         if(server_name == 'chril2-web'):
-            conn.compute.wait_for_server(server)
-            if( len(conn.compute.get_server(server.id)['addresses']['chril2-net']) < 2 ):
-                floating_ip = conn.network.create_ip(floating_network_id=public_net.id)
-                conn.compute.add_floating_ip_to_server(server, floating_ip.floating_ip_address)
-                print(f'Added address {floating_ip["floating_ip_address"]}')
-            else:
-                print('Chril2-web already has a floating IP address')
-    pass
+            utilities.add_floating_ip_to_server(server_name, public_net)
 
 def run():
     ''' Start  a set of Openstack virtual machines
@@ -95,7 +30,6 @@ def run():
         else:
             print(f'\nStarting server {server_name}...')
             conn.compute.start_server(server)
-    pass
 
 def stop():
     ''' Stop  a set of Openstack virtual machines
@@ -108,20 +42,13 @@ def stop():
         else:
             print(f'\nStopping server {server_name}...')
             conn.compute.stop_server(server)
-    pass
 
 def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
-
     for server_name in SERVERS:
-        server = conn.compute.find_server(server_name)
-        if( server != None):
-            print(f'\nDeleting server {server_name}...')
-            conn.compute.delete_server(server)
-        else:
-            print(f'\nServer {server_name} does not exist - skipping')
+        utilities.destroy_server(server_name)
     
     subnet = conn.network.find_subnet(SUBNET_NAME)
     router = conn.network.find_router(ROUTER_NAME)
@@ -144,7 +71,6 @@ def destroy():
         conn.network.delete_network(network)
     else:
         print(f'\nNetwork {NETWORK_NAME} does not exist - skipping')
-    pass
 
 def status():
     ''' Print a status report on the OpenStack
@@ -157,7 +83,6 @@ def status():
         else:
             print(f'\nGettings status of server {server_name}...')
             print(server.status)
-    pass
 
 
 ### You should not modify anything below this line ###
---
+++ utilities.py
@@ -0,0 +1,98 @@
+import openstack
+
+KEYPAIR = 'sysadminapp'
+conn = openstack.connect(could_name='openstack')
+
+IMAGE = 'ubuntu-minimal-16.04-x86_64'
+FLAVOUR = 'c1.c1r1'
+SECURITY_GROUP = 'default'
+
+SUBNET_IP_VERSION = 4
+SUBNET_CIDR = '192.168.50.0/24'
+
+def create_network(network_name):
+    network = conn.network.find_network(network_name)
+    if(network == None):
+        print(f'\nCreating network {network_name}...')
+        network = conn.network.create_network(name=network_name)
+    else:
+        print(f'\nNetwork {network_name} already exists - skipping')
+    return network
+
+def create_subnet(subnet_name, network):
+    subnet = conn.network.find_subnet(subnet_name)
+    if(subnet == None):
+        print(f'\nCreating subnet {subnet_name}...')
+        subnet = conn.network.create_subnet(
+            name=subnet_name, network_id=network.id, ip_version=SUBNET_IP_VERSION, cidr=SUBNET_CIDR)
+    else:
+        print(f'\nSubnet {subnet_name} already exists - skipping')
+    return subnet
+
+def create_router(router_name, subnet, public_network):
+    router = conn.network.find_router(router_name)
+    if (router == None):
+        print(f'\nCreating router {router_name}...')
+        router = conn.network.create_router(name=router_name, external_gateway_info={
+                                            'network_id': public_network.id})
+        router = conn.network.add_interface_to_router(router, subnet.id)
+    else:
+        print(f'\nRouter {router_name} already exists - skipping')
+
+def create_server(server_name, network):
+    image = conn.compute.find_image(IMAGE)
+    if(image == None):
+        print(f'\nCOULD NOT FIND IMAGE {IMAGE}')
+
+    flavour = conn.compute.find_flavor(FLAVOUR)
+    if(flavour == None):
+        print(f'\nCOULD NOT FIND FLAVOUR {FLAVOUR}')
+
+    keypair = conn.compute.find_keypair(KEYPAIR)
+    if(keypair == None):
+        print(f'\nCOULD NOT FIND KEYPAIR {KEYPAIR}')
+
+    security_group = conn.network.find_security_group(SECURITY_GROUP)
+    if(security_group == None):
+        print(f'\nCOULD NOT FIND SECURITY GROUP {SECURITY_GROUP}')
+
+    server = conn.compute.find_server(server_name)
+    if(server == None):
+        print(f'\nCreating server {server_name}...')
+        server = conn.compute.create_server(
+            name=server_name, image_id=image.id,
+            # Should all have IP addresses?
+            flavor_id=flavour.id, networks=[{'uuid': network.id}],
+            key_name=keypair.name, security_groups=[
+                {'sgid': security_group.id}]
+        )
+    else:
+        print(f'\nServer {server_name} already exists - skipping')
+
+def destory_server(server_name):
+    server = conn.compute.find_server(server_name)
+    if( server != None):
+        print(f'\nDeleting server {server_name}...')
+        conn.compute.delete_server(server)
+    else:
+        print(f'\nServer {server_name} does not exist - skipping')
+
+def find_public_network(public_network_name):
+    public_net = conn.network.find_network(public_network_name)
+    if(public_net == None):
+        print(f'\nCOULD NOT FIND NETWORK {public_network_name}')
+    return public_net
+
+def add_floating_ip_to_server(server_name, network):
+    server = conn.compute.find_server(server_name)
+    conn.compute.wait_for_server(server)
+    if(server == None):
+        print(f'\nCOULD NOT FIND SERVER {server_name}')
+
+    if(len(conn.compute.get_server(server.id)['addresses'][server_name]) < 2):
+        floating_ip = conn.network.create_ip(floating_network_id=network.id)
+        conn.compute.add_floating_ip_to_server(
+            server, floating_ip.floating_ip_address)
+        print(f'Added address {floating_ip["floating_ip_address"]}')
+    else:
+        print('Chril2-web already has a floating IP address')
