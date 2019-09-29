9259e5cb11065fcf54d44be62a8560eee9be3839
yandong <yandongqiao@gmail.com>
2019-09-30 10:44:49 +1300

    Finish destory function

--- assignment.py
+++ assignment.py
@@ -1,3 +1,8 @@
+"""
+API reference
+https://docs.openstack.org/openstacksdk/latest/user/connection.html
+https://docs.openstack.org/openstacksdk/latest/user/proxies/compute.html
+"""
 import argparse
 import openstack
 import utils
@@ -22,28 +27,33 @@ network = conn.network.find_network(my_network_name)
 keypair = conn.compute.find_keypair(KEYPAIR)
 security_group = conn.network.find_security_group(SECURITYGROUP)
 public_net = conn.network.find_network(PUBLICNET)
+router = conn.network.find_router(my_router_name)
+subnet = conn.network.find_subnet(my_subnet_name)
+get_web_server = conn.get_server(name_or_id=server_list[0])
+find_web_server = conn.compute.find_server(server_list[0])
 
 
 def create():
     """ Create a set of Openstack resources.
     If the required resources do not exist, new one will be created.
     """
-    if not conn.network.find_network(my_network_name):
+    if not network:
         netw = utils.create_network(conn, my_network_name)
     else:
-        netw = conn.network.find_network(my_network_name)
+        netw = network
         print("Network %s exists already." % my_network_name)
 
-    if not conn.network.find_subnet(my_subnet_name):
+    if not subnet:
         subn = utils.create_subnet(conn, netw, my_subnet_name, my_cidr)
     else:
-        subn = conn.network.find_subnet(my_subnet_name)
+        subn = subnet
         print("Subnet %s exists already." % my_subnet_name)
 
-    if not conn.network.find_router(my_router_name):
+    if not router:
         rout = utils.create_router(conn, my_router_name, public_net)
         utils.add_router_interface(conn, rout, subn)
     else:
+        rout = router
         print("Router %s exists already." % my_router_name)
 
     """ Check whether the provided resources exist. If they do not exist
@@ -60,8 +70,7 @@ def create():
         for server in server_list:
             n_serv = conn.compute.find_server(server)
             if not n_serv:
-                print("------------Creating server %s...--------" % server)
-                print("network is %s " % conn.network.find_network(my_network_name))
+                print("------------ Creating server %s... --------" % server)
                 n_serv = conn.compute.create_server(
                     name=server,
                     image_id=image.id,
@@ -75,35 +84,81 @@ def create():
                 print("Server %s exists already" % server)
 
         # Checking, creating and attaching floating ip to web server
-        get_web_server = conn.get_server(name_or_id=server_list[0])
         if len(get_web_server["addresses"][my_network_name]) < 2:
             print(
-                "-------- Creating and attaching floating ip to server %s"
+                "-------- Creating and attaching floating ip to server %s --------"
                 % server_list[0]
             )
-            web_server = conn.compute.find_server(server_list[0])
-            conn.create_floating_ip(network=PUBLICNET, server=web_server)
+
+            conn.create_floating_ip(network=PUBLICNET, server=find_web_server)
+        else:
+            print("Floating ip is attached to server %s already" % server_list[0])
 
 
 def run():
     """ Start  a set of Openstack virtual machines
     if they are not already running.
     """
-    pass
+    for server in server_list:
+        get_server = conn.get_server(name_or_id=server)
+        if get_server:
+            if get_server.status not in ["Active"]:
+                print("------- Starting server %s... --------" % server)
+                conn.compute.start_server(server)
+                conn.compute.wait_for_server(conn.compute.find_server(server))
+            else:
+                print("Server %s is running already" % server)
+        else:
+            print(
+                "Server %s can not be got. Please check wthether the server exists."
+                % server
+            )
 
 
 def stop():
     """ Stop  a set of Openstack virtual machines
     if they are running.
     """
-    pass
 
 
 def destroy():
     """ Tear down the set of Openstack resources
     produced by the create action
     """
-    pass
+
+    # Detach and releasing floating ip for the web server
+    web_server_ip = get_web_server["interface_ip"]
+    list_ips = conn.list_floating_ips()
+    for ipa in list_ips:
+        if ipa["floating_ip_address"] == web_server_ip:
+            ip_id = ipa["id"]
+            break
+    if ip_id:
+        print(
+            "Detaching floating ip with id %s from server %s" % (ip_id, server_list[0])
+        )
+        conn.detach_ip_from_server(find_web_server.id, ip_id)
+        conn.delete_floating_ip(ip_id, retry=3)
+    # Delete servers one by one
+    for server in server_list:
+        find_server = conn.compute.find_server(server)
+        if find_server:
+            print("------ Deleteing server %s" % server)
+            conn.compute.delete_server(find_server)
+    # Delete router
+    if router:
+        print("------ Removing interface from router %s... -------" % my_router_name)
+        conn.network.remove_interface_from_router(router, subnet.id)
+        print("------ Deleteing router %s...--------" % my_router_name)
+        conn.network.delete_router(router)
+    # Delete Subnet
+    if subnet:
+        print("------ Deleteing subnet %s... ---------" % my_subnet_name)
+        conn.network.delete_subnet(subnet)
+    # Delete network
+    if network:
+        print("------ Deleteing network %s... ---------" % my_network_name)
+        conn.network.delete_network(network)
 
 
 def status():
