b8d63ebdc7867eee8e5d912574268cfd829f403e
clarjc3 <clarjc3@student.op.ac.nz>
2019-10-08 04:52:02 +0000

    trying to create router

--- assignment.py
+++ assignment.py
@@ -7,12 +7,21 @@ conn = openstack.connect(cloud_name=’openstack’)
 IMAGE = 'ubuntu-minimal-16.04-x86_64'
 FLAVOUR = 'c1.c1r1'
 NETWORK = 'clarjc3-net'
+ROUTER = 'clarjc3-rtr'
 KEYPAIR = 'clarjc3-key'
 
 def create():
     ''' Create a set of Openstack resources '''
-    #  Create Network
+    #  Create Network and Subnet
     openstack network create --internal NETWORK
+    openstack subnet create --network NETWORK --subnet-range 192.168.50.0/24 clarjc3-subnet
+    #  Create Router
+    openstack router create ROUTER
+    openstack router add subnet ROUTER clarjc3-subnet
+    openstack router set --external-gateway public-net ROUTER
+    #  Create Server
+    
+    
     pass
 
 def run():
