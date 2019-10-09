42ca8bfeb0b70edd2b47ab8e21816585121b809a
clarjc3 <clarjc3@student.op.ac.nz>
2019-10-09 09:00:22 +0000

    trying to do everything the right way lol rip

--- assignment.py
+++ assignment.py
@@ -3,24 +3,64 @@ import openstack
 
 #  Connect to the openstack service
 conn = openstack.connect(cloud_name=’openstack’)
-
+#  Creating resource name variables
 IMAGE = 'ubuntu-minimal-16.04-x86_64'
 FLAVOUR = 'c1.c1r1'
-NETWORK = 'clarjc3-net'
-ROUTER = 'clarjc3-rtr'
 KEYPAIR = 'clarjc3-key'
+PUBLIC_NETWORK = 'public-net'
+NETWORK = 'clarjc3-net'
+SUBNET = 'clarjc3-subnet'
+ROUTER = 'clarjc3-rtr'
+SECURITY_GROUP='assignment2'
 
 def create():
     ''' Create a set of Openstack resources '''
-    #  Create Network and Subnet
-    openstack network create --internal NETWORK
-    openstack subnet create --network NETWORK --subnet-range 192.168.50.0/24 clarjc3-subnet
-    #  Create Router
-    openstack router create ROUTER
-    openstack router add subnet ROUTER clarjc3-subnet
-    openstack router set --external-gateway public-net ROUTER
-    #  Create Server
+    #  Get resources from the cloud
+    image = conn.compute.find_image(IMAGE)
+    flavour = conn.compute.find_flavour(FLAVOUR)
+    keypair = conn.compute.find_keypair(KEYPAIR)
+    security_group = conn.network.find_security_group(SECURITY_GROUP)
     
+    #  Create Network
+    network = conn.network.find_network(NETWORK)
+    if not network:
+        network = conn.network.create_network(name=NETWORK)
+    
+    #  Create Subnet
+    subnet = conn.network.find_subnet(SUBNET)
+    if not subnet:
+        subnet = conn.network.create_subnet(
+            name=SUBNET,
+            network_id=network.id,
+            ip_version=4,
+            cidr='192.168.50.0/24',
+            gateway_ip='192.168.50.1')
+    
+    #  Create Router
+    public_network = conn.network.find_network(PUBLIC_NETWORK)
+    router = conn.network.find_router(ROUTER)
+    if not router:
+        router = conn.network.create_router(
+            name=ROUTER,
+            external_gateway_info={"network_id": public_network.id})
+        conn.network.router_add_interface(router, subnet.id)
+    
+    #  Create floating IP address
+    floating_ip = conn.network.create_ip(floating_network_id=network.id)
+    
+    #  Create Server
+    SERVER = 'clarjc3-server'
+    #clarjc3-web, clarjc3-app, clarjc3-db
+    server = conn.compute.find_server(SERVER)
+    if not server:
+        server = conn.compute.create_server(
+            name=SERVER,
+            image_id=image.id,
+            flavour_id=flavour.id,
+            networks=[{"uuid": network.id}],
+            key_name=keypair.name)
+        server = conn.compute.wait_for_server(server)
+        conn.compute.add_floating_ip_to_server(server, floating_ip.floating_ip_address) #  Assign floating IP to web server
     
     pass
 
@@ -42,6 +82,21 @@ def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
+    #  Delete Server
+    
+    #  Delete Floating IP
+    
+    #  Delete Router
+    router = conn.network.find_router(ROUTER)
+    if router:
+        conn.network.
+        conn.network.delete_router(ROUTER)
+    #  Delete Network
+    network = conn.network.find_network(NETWORK)
+    if network:
+        for subnet in network.subnet_ids:
+            conn.network.delete_subnet(subnet)
+        conn.network.delete_network(network)
     
     pass
 
