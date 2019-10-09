51feaa829a465da6f7774e86eb41a2177120b535
James Grigg <grigjm1@student.op.ac.nz>
2019-10-09 16:29:42 +1300

    Added in initial code for the create and run functions

--- assignment.py
+++ assignment.py
@@ -6,6 +6,7 @@ import argparse
 import openstack
 
 conn = openstack.connect(cloud_name='openstack')
+public_net = conn.network.find_network('public-net')
 
 IMAGE = 'ubuntu-minimal-16.04-x86_64'
 FLAVOUR = 'c1.c1r1'
@@ -17,12 +18,43 @@ SERVERLIST = { 'grigjm1-web', 'grigjm1-app', 'grigjm1-db' }
 
 def create():
     ''' Create a set of Openstack resources '''
+	
+	# Find network, subnet and router first
+    network = conn.network.find_network(NETWORK)
+    subnet = conn.network.find_subnet('grigjm1-subnet')
+    router = conn.network.find_router('grigjm1-rtr')
+
+    # Create network if it is not found
+    if network:
+        print("Network resource already exist")
+    else:
+        network = conn.network.create_network(name=NETWORK)
+        print("Network resource created")
+		
     pass
 
 def run():
     ''' Start  a set of Openstack virtual machines
     if they are not already running.
     '''
+	
+	# Check every server with names inside SERVERLIST list
+    # and turn its status to ACTIVE when it is SHUTOFF
+	
+    for server in SERVERLIST:
+        check_server = conn.compute.find_server(server)
+        if check_server:
+            check_server = conn.compute.get_server(check_server)
+            if check_server.status == "ACTIVE":
+                print("Server [" + server + "] is already active")
+            elif check_server.status == "SHUTOFF":
+                print("Server [" + server + "] is not active")
+                print("Starting server [" + server + "]......")
+                conn.compute.start_server(check_server)
+                print("Server [" + server + "] is active")
+        else:
+            print("Server [" + server + "] does not exist")
+			
     pass
 
 def stop():
