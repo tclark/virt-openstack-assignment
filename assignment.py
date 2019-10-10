ea43ae492eb14a77b709a872767bfe84d13c453e
shinrl1 <shinrl1@student.op.ac.nz>
2019-10-10 15:30:54 +0000

    network creating

--- assignment.py
+++ assignment.py
@@ -1,8 +1,31 @@
 import argparse
 import openstack
 
+
+#create connection to openstack
+conn = openstack.connect(cloudname='openstack')
 def create():
-    ''' Create a set of Openstack resources '''
+    
+    #collect variables
+    flavour = conn.compute.find_flavor('c1.c1r1')
+    image = conn.compute.find_image('ubuntu-minimal-16.04-x86_64')
+    keypair = conn.compute.find_keypair('kate')
+    network = conn.network.find_network('shinrl1-net')
+    router = conn.network.find_router('shinrl1-rtr')
+
+    #create network
+
+    if not network:
+        network = conn.network.create_network(name='shinrl1-net')
+        print(network)
+        subnet='shinrl1-subnet',
+        network_id=network.id,
+        ip_version='4',
+        cidr='192.168.50.0/24'
+        gateway_ip='192.168.50.1'
+        print("Network created ^_^")
+    else:
+        print("Borked")
     pass
 
 def run():
@@ -27,6 +50,12 @@ def status():
     ''' Print a status report on the OpenStack
     virtual machines created by the create action.
     '''
+    def list_networks(conn):
+        print("List Networks:")
+
+    for network in conn.network.networks():
+        print(network)
+
     pass
 
 
