452556ecaa98a9b0d967e783f1a557213cc6e5d7
hayeska2 <48660806+hayeska2@users.noreply.github.com>
2019-10-11 14:35:09 +1300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -15,6 +15,58 @@ conn = openstack.connect(cloud_name='openstack')
 
 def create():
     ''' Create a set of Openstack resources '''
+   # Create the Network
+    if conn.network.find_network('hayeska2-net') is None:
+        network = conn.network.create_network(
+            name='hayeska2-net')
+        print("Network has successfully been created.")
+    else:
+        print("Network already exists.")
+        pass
+    # Create the Subnet
+     if conn.network.find_subnet('hayeska2-subnet') is None:
+        subnet = conn.network.create_subnet(
+            name='hayeska2-subnet',
+            network_id=network.id,
+            ip_version='4',
+            cidr='192.168.50.0/24',
+            gateway_ip='192.168.50.1')
+        print("Subnet has been successfully created.")
+    else:
+        print("Subnet already exists.")
+        pass
+    
+    # Create the Router
+     if conn.network.find_router('hayeska2-router') is None:
+        public_net = conn.network.find_network(name_or_id='public-net')
+        router = conn.network.create_router(
+            name='hayeska2-router',
+            external_gateway_info={ 'network_id' : public_net.id }
+        )
+        print("Router has been successfully created.")
+    else:
+        print("Router already exists.")
+        pass
+    
+    # Create the server
+     for server in server_list:
+        server = conn.compute.find_server(server)
+        if server is None:
+            print("Creating " + server + " the server")
+            server = conn.compute.create_server(name=serv, image_id=image.id, flavor_id=flavour.id, networks=[{"uuid": network.id}], key_name=keypair.name)
+            server = conn.compute.wait_for_server(server)
+            conn.compute.add_security_group_to_server(server, sec_group)
+            print(server + " Server has been created")
+        else:
+            print(server + " The server already exists")
+            pass
+        
+        # Create and Assign floating IP address
+          server = conn.compute.find_server('hayeska2-web')
+          floating_ip = conn.network.create_ip(floating_network_id=public_net.id)
+          conn.compute.add_floating_ip_to_server(server, floating_ip.floating_ip_address)
+
+
     pass
 
 def run():
