7164821bab54205aea2362ac8421fd1e323be97a
woodjj1 <woodjj1>
2019-10-11 01:36:46 +0000

    accidently deleted other funtions for assignment.py

--- assignment.py
+++ assignment.py
@@ -45,3 +45,46 @@ def create():
         )
         print("Subnet created")
 
+    def run():
+    ''' Start  a set of Openstack virtual machines
+    if they are not already running.
+    '''
+    pass
+
+def stop():
+    ''' Stop  a set of Openstack virtual machines
+    if they are running.
+    '''
+    pass
+
+def destroy():
+    ''' Tear down the set of Openstack resources 
+    produced by the create action
+    '''
+    pass
+
+def status():
+    ''' Print a status report on the OpenStack
+    virtual machines created by the create action.
+    '''
+    pass
+
+
+### You should not modify anything below this line ###
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('operation',
+                        help='One of "create", "run", "stop", "destroy", or "status"')
+    args = parser.parse_args()
+    operation = args.operation
+
+    operations = {
+        'create'  : create,
+        'run'     : run,
+        'stop'    : stop,
+        'destroy' : destroy,
+        'status'  : status
+        }
+
+    action = operations.get(operation, lambda: print('{}: no such operation'.format(operation)))
+    action()
