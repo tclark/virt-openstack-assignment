1171e94c977db659433de552b70f99ed628891d2
woodjj1 <woodjj1>
2019-10-11 02:17:08 +0000

    added router and servers code

--- assignment.py
+++ assignment.py
@@ -24,16 +24,16 @@ def create():
     subnet = conn.network.find_subnet('woodjj1-subnet')
     router = conn.network.find_router('woodjj1-rtr')
 
-    # Create network when its name can not be found on catalyst cloud
+    # Create network if it doesn't exist
     if network:
-        print("Network resource already exist")
+        print("Network already exists")
     else:
         network = conn.network.create_network(
             name=NETWORK
         )
-        print("Network resource created")
+        print("Network created")
 
-    # Create subnet when its name can not be found on catalyst cloud
+    # Create subnet if it doesn't exist
     if subnet:
         print("Subnet already exists")
     else:
@@ -44,6 +44,49 @@ def create():
             cidr=SUBNET
         )
         print("Subnet created")
+
+         # Create router if it doesn't exist
+    if router:
+        print("Router already exists")
+    else:
+        router = conn.network.create_router(
+            name='woodjj1-rtr',
+            external_gateway_info={'network_id': public_net.id})
+        conn.network.add_interface_to_router(router, subnet.id)
+        print("Router resource created")
+
+    # Launch Instances
+    image = conn.compute.find_image(IMAGE)
+    flavour = conn.compute.find_flavor(FLAVOUR)
+    keypair = conn.compute.find_keypair(KEYPAIR)
+    security = conn.network.find_security_group(SECURITY)
+
+    # Create servers if they don't exist
+    for server in SERVERLIST:
+        check_server = conn.compute.find_server(server)
+        if check_server:
+            print("Servers already exist")
+        else:
+            new_server = conn.compute.create_server(
+                name=server,
+                image_id=image.id,
+                flavor_id=flavour.id,
+                networks=[{"uuid": network.id}],
+                key_name=keypair.name,
+                security_groups=[{"name": security.name}]
+            )
+            new_server = conn.compute.wait_for_server(new_server)
+            print("Server [" + server + "] created")
+
+            # Associate Floating IP address to woodjj1-web
+            if server == 'woodjj1-web':
+                print("Associating floating IP address to WEB server......")
+                floating_ip = conn.network.create_ip(floating_network_id=public_net.id)
+                web_server = conn.compute.find_server('woodjj1-web')
+                conn.compute.add_floating_ip_to_server(web_server, floating_ip.floating_ip_address)
+                print("Floating IP address has been allocated to WEB server")
+
+    pass
 
     def run():
     ''' Start  a set of Openstack virtual machines
