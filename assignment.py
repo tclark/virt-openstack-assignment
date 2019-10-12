44f3b003aece2f2b10fecd31d1d9fcca937c364b
Hua WANG <wangh21@student.op.ac.nz>
2019-10-12 16:40:39 +1300

    update

--- assignment.py
+++ assignment.py
@@ -139,12 +139,15 @@ def destroy():
                 # Finds floating ip address of web server and deletes it first.
                 conn.network.delete_ip(conn.network.find_ip(
                     conn.compute.get_server(server).addresses[NETWORK][1]["addr"]))
-                conn.network.delete_ip(conn.network.find_ip(
-                    conn.compute.get_server(server).addresses[NETWORK][0]["addr"]))
             conn.compute.delete_server(s)
         else:
             print(f'Server {server} does not exists. skip...')
 
+    if router:
+        print(f'clearing router interface...')
+        conn.network.remove_interface_from_router(router, subnet.id)
+        conn.network.delete_router(router)
+        
     if network:
         print(f'clearing subnet interface...')
         conn.network.delete_subnet(subnet)
@@ -152,10 +155,6 @@ def destroy():
         conn.network.delete_network(network)
         print(f'Operation completed.')
 
-    if router:
-        print(f'clearing router interface...')
-        conn.network.remove_interface_from_router(router, subnet.id)
-        conn.network.delete_router(router)
 
 def status():
     ''' Print a status report on the OpenStack
