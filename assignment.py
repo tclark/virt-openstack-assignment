4398c9a3d10848c0391eb5f05c93fced142d83ba
Hua WANG <wangh21@student.op.ac.nz>
2019-10-12 17:42:26 +1300

    update

--- assignment.py
+++ assignment.py
@@ -151,9 +151,11 @@ def destroy():
         conn.network.remove_interface_from_router(router, subnet.id)
         conn.network.delete_router(router, ignore_missing=True)
 
-    if network:
+    if subnet:
         print(f'clearing subnet interface...')
         conn.network.delete_subnet(subnet, ignore_missing=True)
+
+    if network:
         print(f'clearing network interface...')
         conn.network.delete_network(network, ignore_missing=True)
         print(f'Operation completed.')
