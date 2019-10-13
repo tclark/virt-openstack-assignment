39ba008e0dcdd2be37cfdce6136238c59e2384fa
lunarised <j@p7.co.nz>
2019-10-13 16:49:27 +1300

    Rewrote alot and fixed formatting

--- assignment.py
+++ assignment.py
@@ -26,40 +26,40 @@ def create():
     os_network = os_connect.network.find_network(NETWORK)
     
     
-    if os_network:
-        print(NETWORK, 'already exists! Skipping...')
+    if os_network != None:
+        print('%s already exists! Skipping...' % NETWORK)
         
     else:
-        print('Creating', NETWORK)
+        print('Creating %s '% NETWORK)
         os_network = os_connect.network.create_network(name=NETWORK)
 
     os_subnet = os_connect.network.find_subnet(SUBNET)
-    if os_subnet:
-        print(SUBNET, 'already exists')
+    if os_subnet != None:
+        print('%s already exists' % SUBNET)
     else:
-        print('Creating', SUBNET)
+        print('Creating %s ' % SUBNET)
         os_subnet = os_connect.network.create_subnet(name=SUBNET,
                                             cidr='192.168.50.0/24',
                                             ip_version=4,
                                             network_id=os_network.id)
 
     os_router = os_connect.network.find_router(ROUTER)
-    if os_router:
-        print(ROUTER, 'already exists')
+    if os_router != None:
+        print('%s already exists' % ROUTER)
     else:
-        print('Creating', ROUTER)
+        print('Creating %s '  % ROUTER)
         ext = {'network_id': os_public_net.id}
         os_router = os_connect.network.create_router(name=ROUTER,
                                             external_gateway_info=ext)
-        print('Creating interface for', SUBNET)
+        print('Creating interface for %s' % SUBNET)
         os_connect.network.add_interface_to_router(os_router, os_subnet.id)
 
     for server in SERVER_NAMES:
         s = os_connect.compute.find_server(server)
-        if s:
-            print(server, 'already exists')
+        if s != None:
+            print('%s already exists' % server)
         else:
-            print('Creating', server)
+            print('Creating %s' % server)
             s = os_connect.compute.create_server(name=server,
                                            image_id=os_image.id,
                                            flavor_id=os_flavor.id,
@@ -79,7 +79,7 @@ def create():
             if a['OS-EXT-IPS:type'] == 'floating':
                 addrs.append(a['addr'])
         floatingips.extend(addrs)
-    #Closes the program if none are avaliable
+    #Closes the program if no floating IPS are avaliable
     if floatingips.__len__ == 0:
         print("There are no floating ips avalible")
         exit
@@ -98,25 +98,125 @@ def run():
     ''' Start  a set of Openstack virtual machines
     if they are not already running.
     '''
-    pass
+    for server in SERVER_NAMES:
+        s = os_connect.compute.find_server(server)
+        if s == None:
+            print(server, 'not found, skipping!')
+        else:
+            s = os_connect.compute.get_server(s)
+            print('Starting %s... ' % server, end='')
+            try:
+                os_connect.compute.start_server(s.id)
+            except openstack.exceptions.ConflictException:
+                print('The Server is Currently Running!, skipping!')
+            else:
+                os_connect.compute.wait_for_server(s)
+                print('OK')
+
 
 def stop():
     ''' Stop  a set of Openstack virtual machines
     if they are running.
     '''
-    pass
+    for server in SERVER_NAMES:
+        s = os_connect.compute.find_server(server)
+        if s == None:
+            print('%s not found, skipping!' % server)
+        else:
+            s = os_connect.compute.get_server(s.id)
+            print('Shutting off %s... ' % server, end='')
+            try:
+                os_connect.compute.stop_server(s.id)
+            except openstack.exceptions.ConflictException:
+                print('%s was already shut off!' % server)
+            else:
+                os_connect.compute.wait_for_server(s, status='SHUTOFF')
+                print('OK')
+
 
 def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
-    pass
+    os_subnet = os_connect.network.find_subnet(SUBNET)
+    for server in SERVER_NAMES:
+        s = os_connect.compute.find_server(server)
+        if s != None:
+            s = os_connect.compute.get_server(s)
+            floatingips = []
+            for net in s.addresses:
+                for addr in s.addresses[net]:
+                    addrs = []
+                    if addr['OS-EXT-IPS:type'] == 'floating':
+                        addrs.append(addr['addr'])
+                floatingips.extend(addrs)
+
+
+
+
+
+            for ip in floatingips:
+                os_addr = os_connect.network.find_ip(ip)
+                print('Releasing floating IP', ip)
+                os_connect.network.delete_ip(os_addr)
+            print('Deleting', server)
+            os_connect.compute.delete_server(s, ignore_missing=True)
+            print('Waiting for %s to be deleted' % server)
+            while s:
+                s = os_connect.compute.find_server(server)
+        else:
+            print('Unable to find %s, skipping' % server)
+    os_router = os_connect.network.find_router(ROUTER)
+    if os_router:
+        print('Deleting interface for', SUBNET)
+        os_connect.network.remove_interface_from_router(os_router, os_subnet.id)
+        print('Deleting', ROUTER)
+        os_connect.network.delete_router(os_router, ignore_missing=True)
+    else:
+        print('Unable to find %s skipping' % ROUTER)
+
+    # Delete network if it exists.
+    os_network = os_connect.network.find_network(NETWORK)
+    if os_network:
+        print('Deleting', SUBNET)
+        os_connect.network.delete_subnet(os_subnet, ignore_missing=True)
+        print('Deleting', NETWORK)
+        os_connect.network.delete_network(os_network, ignore_missing=True)
+    else:
+        print('Unable to find %s , skipping' % NETWORK)
+
 
 def status():
     ''' Print a status report on the OpenStack
     virtual machines created by the create action.
     '''
-    pass
+    servers = []
+    # Get info for  servers.
+    for name in SERVER_NAMES:
+        print('Fetching status of %s...' % name , end=' ')
+        s = os_connect.compute.find_server(name)
+        # Get full server info.
+        if s:
+            s = os_connect.compute.get_server(s.id)
+            servers.append(s)
+            print('OK')
+        else:
+            print('NOT FOUND')
+
+    # Display status of known and found servers.
+    for server in servers:
+        print('\n%s: %s' % server.name, server.status)
+        iplist = []
+        for net in server.addresses:
+            for a in server.addresses[net]:
+                iplist.append(a['addr'])
+        if iplist:
+            print('IP Addresses:')
+            for i in iplist:
+                print(i)
+        else:
+            print('No IP\'s were found!')
+
 
 
 ### You should not modify anything below this line ###
