a57fa9d8ed93b5eae33a6e1edcf5f94a232e1510
shinrl1 <shinrl1@student.op.ac.nz>
2019-10-13 02:05:45 +0000

    subnet issue fixed, find variables named properly

--- assignment.py
+++ assignment.py
@@ -7,20 +7,19 @@ conn = openstack.connect(cloudname='openstack')
 def create():
     
     #collect variables
-    flavour = conn.compute.find_flavor('c1.c1r1')
-    image = conn.compute.find_image('ubuntu-minimal-16.04-x86_64')
-    keypair = conn.compute.find_keypair('assign')
-    network = conn.network.find_network('shinrl1-net')
-    router = conn.network.find_router('shinrl1-rtr')
-    public_net = conn.network.find_network('public-net')
-    subnet = conn.network.find_subnet('shinrl1-subnet')
-    cidr = "192.168.50.0/24"
-    webserver = conn.compute.find_server('shinrl1-web')
-    security_group = conn.network.find_security_group('assignment2')
-    router = conn.network.find_router('shinrl1-rtr')
+    n_flavour = conn.compute.find_flavor('c1.c1r1')
+    n_image = conn.compute.find_image('ubuntu-minimal-16.04-x86_64')
+    n_keypair = conn.compute.find_keypair('assign')
+    n_network = conn.network.find_network('shinrl1-net')
+    n_public_net = conn.network.find_network('public-net')
+    n_subnet = conn.network.find_subnet('shinrl1-subnet')
+    CIDR = "192.168.50.0/24"
+    n_webserver = conn.compute.find_server('shinrl1-web')
+    n_security_group = conn.network.find_security_group('assignment2')
+    n_router = conn.network.find_router('shinrl1-rtr')
 
     #create network
-    if not network:
+    if not n_network:
         network = conn.network.create_network(name='shinrl1-net')
         print(network)
         subnet='shinrl1-subnet',
@@ -33,32 +32,32 @@ def create():
         print("Network Borked")
     
     #create subnet
-    if not subnet: 
+    if not n_subnet: 
         subnet = conn.network.create_subnet(name='shinrl1-subnet',
-                network_id=network.id, ip_version='4', cidr=cidr)
+                network_id=network.id, ip_version='4', cidr=CIDR)
         print("Subnet Created")
     else:
         print("Subnet borked") 
     #create router
-    if not router:
+    if not n_router:
     
-        args = {'name': "shinrl1-rtr", 'external_gateway_info': {'network_id': public_net.id}}
+        args = {'name': "shinrl1-rtr", 'external_gateway_info': {'network_id': n_public_net.id}}
         router = conn.network.create_router(**args)
-        conn.network.add_interface_to_router(router, subnet_id=subnet.ids[0])
+        conn.network.add_interface_to_router(n_router, subnet_id=n_subnet.ids[0])
 
         print("Router Created ^_^")
     else:
         print ("Router Borked")
 
     #reaete servers
-    if not webserver:
-        conn.network.add_interface_to_router(router, subnet_id=network.subnet_ids[0])
-        webserver = conn.compute.create_server(name="shinrl1-web", image_id=image.id,  flavor_id=flavour.id, networks=[{"uuid": network.id}], key_name=keypair.name)
+    if not n_webserver:
+        conn.network.add_interface_to_router(n_router, subnet_id=n_network.subnet_ids[0])
+        webserver = conn.compute.create_server(name="shinrl1-web", image_id=n_image.id,  flavor_id=n_flavour.id, networks=[{"uuid": n_network.id}], key_name=n_keypair.name)
         webserver = conn.compute.wait_for_server(webserver)
-        conn.compute.add_security_group_to_server(webserver, security_group)
+        conn.compute.add_security_group_to_server(webserver, n_security_group)
         print("shinrl1-web up")
     #add floating ips to servers
-        webserver_ip = conn.network.create_ip(floating_network_id=public_net.id)
+        webserver_ip = conn.network.create_ip(floating_network_id=n_public_net.id)
         conn.compute.add_floating_ip_to_server(webserver, address=webserver_ip.floating_ip_address)
         print ("ip attatched to web server:", webserver_ip.floating_ip_address)
 
