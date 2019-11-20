6f3a3bafb187224771d3f9b0cedc1a3341584b1a
dackerss <dackja1@student.op.ac.nz>
2019-11-20 04:32:16 +0000

    Fixed interface and private network issues

--- assignment.py
+++ assignment.py
@@ -1,33 +1,52 @@
 import argparse
 import openstack
+import time
 
 
 global conn 
 conn = openstack.connect(cloud_name='openstack')
 global serverList
 serverList = ["dackja1-app", "dackja1-db", "dackja1-web"]
+IMAGE = 'ubuntu-minimal-16.04-x86_64'
+FLAVOUR = 'c1.c1r1'
+NETWORK = 'dackja1-net'
+KEYPAIR = 'dackja1-key'
+SECURITY_GROUP = 'assignment2'
+SUBNET = 'dackja1-subnet'
+ROUTER = 'dackja1-rtr'
+PUBLICNET = 'public-net'
+
+subnet = conn.network.find_subnet(SUBNET)
+image = conn.compute.find_image(IMAGE)
+flavour = conn.compute.find_flavor(FLAVOUR)
+keypair = conn.compute.find_keypair(KEYPAIR)
+security_group= conn.network.find_security_group(SECURITY_GROUP)
+conn = openstack.connect(cloud_name='openstack')
+
+
 
 def create():
     ''' Create a set of Openstack resources '''
 
     print ("connecting to Openstack")
     conn = openstack.connect(cloud_name='openstack')
+    Network = conn.network.find_network(NETWORK)
 
     print (" creating network, name: dackja1 ")
-    if conn.network.find_network('dackja1-net') is None:
+    if Network is None:
        
-        network=conn.network.create_network(
-            name='dackja1-net')
+        Network=conn.network.create_network(
+            name= NETWORK)
         print("Network created")
     else:
         print("Network dackja1 already exists")
     pass
 
-    if conn.network.find_subnet('dackja1-subnet') is None:
+    if conn.network.find_subnet(SUBNET) is None:
         print ("creating subnet")
         subnet = conn.network.create_subnet(
             name = 'dackja1-subnet',
-            network_id=network.id,
+            network_id=Network.id,
             ip_version='4',
             cidr='192.168.50.0/24',
             gateway_ip='192.168.50.1'
@@ -40,12 +59,13 @@ def create():
 
     print("Creating router")
 
-    if conn.network.find_router('dackja1-rtr') is None:
-        public_net = conn.network.find_network('public-net')
+    if conn.network.find_router(ROUTER) is None:
+        public_net = conn.network.find_network(PUBLICNET)
         router = conn.network.create_router(
-            name='dackja1-rtr',
+            name=ROUTER,
             external_gateway_info={'network_id': public_net.id}
             )
+        conn.network.add_interface_to_router(router, subnet.id)
         print("Router dackja1-rtr has been created")
 
     else:
@@ -54,24 +74,14 @@ def create():
 
         
 
-    IMAGE = 'ubuntu-minimal-16.04-x86_64'
-    FLAVOUR = 'c1.c1r1'
-    Network = 'private-net'
-    KEYPAIR = 'dackja1-key'
-    SECURITY_GROUP = 'assignment2'
 
-    image = conn.compute.find_image(IMAGE)
-    flavour = conn.compute.find_flavor(FLAVOUR)
-    network = conn.network.find_network(Network)
-    keypair = conn.compute.find_keypair(KEYPAIR)
-    security_group= conn.network.find_security_group(SECURITY_GROUP)
 
     for serverName in serverList:
         if conn.compute.find_server(name_or_id=serverName) is None:
             SERVER = serverName
             server =conn.compute.create_server(
             name=SERVER, image_id=image.id, flavor_id=flavour.id,
-            networks=[{"uuid": network.id}], key_name=keypair.name,ecurity_groups=[security_group])
+            networks=[{"uuid": Network.id}], key_name=keypair.name,security_groups=[{'name':security_group.name}])
             server = conn.compute.wait_for_server(server)
             print(serverName + " successfully created.")
         
@@ -127,33 +137,55 @@ def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
-
-    conn = openstack.connect(cloud_name='openstack')
- 
-    network = conn.network.find_network("dackja1-net")
-
-    if network is not None:
-        
-        delNetwork = conn.network.delete_network(network)
-        print("Network Destroyed")
-    else:
-        print("Network does not exist")
+    Network = conn.network.find_network(NETWORK)
 
 
+
+
+    
+
+
+   
+    for server in serverList:
+        ser = conn.compute.find_server(name_or_id=server)
+        if ser is not None:
+            if ser == 'dackja1-web':
+                web = conn.compute.get_server(ser)
+                web_floating_ip = web['addresses'][Network][1]['addr']
+                conn.compute.remove_floating_ip_from_server(web, web_floating_ip)
+                deleteIp = conn.network.delete_ip(deleteIp)
+            conn.compute.delete_server(ser)
+            time.sleep(3)
+           
+            print(ser.name + " Destroyed.")
+        else:
+            print(server + " does not exist.")
+       
     router = conn.network.find_router("dackja1-rtr")
+
+   
     if router is not None:
-        delRouter = conn.network.delete_router(router)
+        conn.network.remove_interface_from_router(router, subnet.id)
+        conn.network.delete_router(router)
         print("Router Destroyed")
     else:
         print("Router does not exist")
 
-    for server in serverList:
-        ser = conn.compute.find_server(name_or_id=server)
-        if ser is not None:
-            conn.compute.delete_server(ser)
-            print(ser.name + " Destroyed.")
-        else:
-            print(server + " does not exist.")
+    if subnet is not None: 
+        conn.network.delete_subnet(subnet) 
+        print('Subnet Destroyed')
+    else:
+        print('Subnet does not exist')
+
+    if Network is not None: 
+        conn.network.delete_network(Network) 
+        print('Network Destroyed') 
+    else:
+        print('Network does not exist')
+
+
+
+   
 
 pass
 
@@ -168,7 +200,7 @@ def status():
             ser = conn.compute.get_server(serverid)
             print("Name: " + ser.name + "\n" 
                 "Status: " + ser.status) 
-            for value in ser.addresses["private-net"]:
+            for value in ser.addresses[NETWORK]:
                 print("IP: " + value["addr"])
             print("\n") 
    
