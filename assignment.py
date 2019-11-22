298ac560157b9e5502e78d1471874ffb329ad379
bradcw1 <bradcw1@student.op.ac.nz>
2019-11-22 00:45:49 +0000

    refactor code

--- assignment.py
+++ assignment.py
@@ -1,11 +1,28 @@
 import argparse
 import openstack
 
-global conn 
 conn = openstack.connect(cloud_name='openstack')
-global serverList
 serverList = [ "bradcw1-app", "bradcw1-db", "bradcw1-web" ]
 
+IMAGE = 'ubuntu-minimal-16.04-x86_64'
+FLAVOUR = 'c1.c1r1'
+NETWORK = 'bradcw1-net'
+SUBNET = 'bradcw1-subnet'
+ROUTER = 'bradcw1-rtr'
+SECURITY_GROUP = 'assignment2'
+KEYPAIR = 'bradcw1-key'
+
+public_net = conn.network.find_network(name_or_id='public-net')
+floating_ip = conn.network.create_ip(floating_network_id=public_net.id)
+router = conn.network.find_router(ROUTER)
+image = conn.compute.find_image(IMAGE)
+flavour = conn.compute.find_flavor(FLAVOUR)
+security_group = conn.network.find_security_group(SECURITY_GROUP)
+keypair = conn.compute.find_keypair(KEYPAIR)   
+
+network = conn.network.find_network(NETWORK)
+router = conn.network.find_router(ROUTER)
+subnet = conn.network.find_subnet(SUBNET)
 
 def create():
 
@@ -14,9 +31,9 @@ def create():
     # CREATE NETWORK
     print("Creating Network...")
     
-    if conn.network.find_network('bradcw1-net') is None:
+    if conn.network.find_network(NETWORK) is None:
         network = conn.network.create_network(
-            name='bradcw1-net')
+            name=NETWORK)
         print("Network successfully created.")
     else:
         print("Network already exists.")
@@ -25,9 +42,9 @@ def create():
     # CREATE SUBNET
     print("Creating Subnet...")
 
-    if conn.network.find_subnet('bradcw1-subnet') is None:
+    if conn.network.find_subnet(SUBNET) is None:
         subnet = conn.network.create_subnet(
-            name='bradcw1-subnet',
+            name=SUBNET,
             network_id=network.id,
             ip_version='4',
             cidr='192.168.50.0/24',
@@ -41,30 +58,19 @@ def create():
     print("Creating Router...")
 
     if conn.network.find_router('bradcw1-rtr') is None:
-        public_net = conn.network.find_network(name_or_id='public-net')
+        
         router = conn.network.create_router(
             name='bradcw1-rtr',
             external_gateway_info={ 'network_id' : public_net.id }
         )
         print("Router successfully created.")
+        conn.network.add_interface_to_router(router, subnet.id)
     else:
         print("Router already exists.")
         pass
 
 
-    # LAUNCH INSTANCES
-
-    IMAGE = 'ubuntu-minimal-16.04-x86_64'
-    FLAVOUR = 'c1.c1r1'
-    NETWORK = 'private-net'
-    SECURITY_GROUP = 'assignment2'
-    KEYPAIR = 'bradcw1-key'
-
-    image = conn.compute.find_image(IMAGE)
-    flavour = conn.compute.find_flavor(FLAVOUR)
-    network = conn.network.find_network(NETWORK)
-    security_group = conn.network.find_security_group(SECURITY_GROUP)
-    keypair = conn.compute.find_keypair(KEYPAIR)    
+    # LAUNCH INSTANCES     
 
     for serverName in serverList:
         if conn.compute.find_server(name_or_id=serverName) is None:
@@ -75,9 +81,9 @@ def create():
             server = conn.compute.wait_for_server(server)
             print(serverName + " successfully created.")
 
-            if serverName == "bradcw1-web":
-                floating_ip = conn.network.create_ip(floating_network_id=public_net.id)    
+            if serverName == "bradcw1-web":   
                 web = conn.compute.find_server("bradcw1-web")
+                conn.compute.wait_for_server(server)
                 conn.compute.add_floating_ip_to_server(web, floating_ip.floating_ip_address)
                 print("Floating IP " + str(floating_ip.floating_ip_address) + " applied to bradcw1-web.")
 
@@ -128,31 +134,53 @@ def destroy():
     produced by the create action
     '''
 
+    # DESTROY SERVERS
+    for server in serverList:
+        ser = conn.compute.find_server(name_or_id=server)
+        
+        if ser is not None:
+            if server == 'bradcw1-web':
+                dserver = conn.compute.get_server(ser)
+                server_floating_ip = dserver['addresses'][NETWORK][1]['addr']
+
+                print("Removing Floating IP")
+                conn.compute.remove_floating_ip_from_server(dserver, server_floating_ip)
+                
+                print("Floating IP removed from " + ser.name)
+                drop_ip = conn.network.find_ip(server_floating_ip)
+                conn.network.delete_ip(drop_ip)
+                
+                print("IP Dropped")
+                
+            conn.compute.delete_server(ser)
+            
+            print(ser.name + " Destroyed.")
+        else:
+            print(server + " does not exist.") 
+
+    # DESTROY ROUTER
+    if router is not None:
+        delInterface = conn.network.remove_interface_from_router(router, subnet.id)
+        delRouter = conn.network.delete_router(router)
+        print("Router Destroyed")
+    else:
+        print("Network does not exist.")        
+
+    # DESTROY SUBNET
+    if subnet is not None:
+        delSubnet = conn.network.delete_subnet(subnet)
+        print("Subnet Destroyed")
+    else:
+        print("Subnet does not exist.")
+    
     # DESTROY NETWORK
     network = conn.network.find_network("bradcw1-net")
     if network is not None:
         delNetwork = conn.network.delete_network(network)
         print("Network Destroyed")
     else:
-        print("Network does not exist.")
-
-    # DESTROY ROUTER
-    router = conn.network.find_router("bradcw1-rtr")
-    if router is not None:
-        delRouter = conn.network.delete_router(router)
-        print("Router Destroyed")
-    else:
-        print("Network does not exist.")
+        print("Network does not exist.")   
     
-    # DESTROY SERVERS
-    for server in serverList:
-        ser = conn.compute.find_server(name_or_id=server)
-        if ser is not None:
-            conn.compute.delete_server(ser)
-            print(ser.name + " Destroyed.")
-        else:
-            print(server + " does not exist.")
-
     pass
 
 def status():
@@ -168,7 +196,7 @@ def status():
             ser = conn.compute.get_server(serverid)
             print("Name: " + ser.name + "\n" 
                 "Status: " + ser.status) 
-            for value in ser.addresses["private-net"]:
+            for value in ser.addresses[NETWORK]:
                 print("IP: " + value["addr"])
             print("\n") 
 
