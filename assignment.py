7f68e2ce8951adb4d7fa79ca5fc9ee893468f4d0
Raymond <huar2@student.op.ac.nz>
2022-09-06 02:16:18 +0000

    Added router

--- assignment.py
+++ assignment.py
@@ -9,6 +9,14 @@ NETWORK = "huar2-net"
 KEYPAIR = "huar2-key"
 SERVERNAME = "huar2-server"
 SUBNET = "huar2-subnet"
+ROUTER = "huar2-router"
+
+WEBSERVER = "huar2-web"
+APPSERVER = "huar2-server"
+DBSERVER = "huar2-db"
+SERVERS = [WEBSERVER, APPSERVER, DBSERVER]
+
+public_net = conn.network.find_network("public-net")
 
 def create():
     ''' Create a set of Openstack resources '''
@@ -18,15 +26,20 @@ def create():
     network = conn.network.find_network(NETWORK)
     keypair = conn.compute.find_keypair(KEYPAIR)
     if not network:
+        print("Creating network: " + NETWORK)
         new_network = conn.network.create_network(name=NETWORK)
-        print(new_network)
+        print("Network " + NETWORK + " created")
+        print("Creating subnet: " + SUBNET)
         new_subnet = conn.network.create_subnet(
         name=SUBNET,
         network_id=new_network.id,
         ip_version='4',
         cidr='192.168.50.0/24',
         gateway_ip='192.168.50.1')
-        print(new_subnet)
+        print("Subnet created")
+        new_router = conn.network.create_router(name=ROUTER, external_gateway_info={"network_id": public_net.id})
+    else:
+        print("Network not created")
     '''if not server:
         server = conn.compute.create_server(name=SERVERNAME, image_id=image.id, flavor_id=flavour.id, networks=[{"uuid": network.id}], key_name=keypair.name, security_groups=[{"name": "lab5-secgrp"}])
         print("Creating VM: " + SERVERNAME)
@@ -95,13 +108,14 @@ def destroy():
     ''' Tear down the set of Openstack resources 
     produced by the create action
     '''
-    print("Delete Network:")
-
     network = conn.network.find_network(NETWORK)
 
     for subnet in network.subnet_ids:
+        print("Deleting subnet ID: " + subnet)
         conn.network.delete_subnet(subnet, ignore_missing=False)
+    print("Deleting network: " + NETWORK)
     conn.network.delete_network(network, ignore_missing=False)
+    print("Network " + NETWORK + " deleted")
 
     '''SERVER = conn.compute.find_server(SERVERNAME)
     
