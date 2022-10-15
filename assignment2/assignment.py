14ee26d48f0d11abcce1f28deb8a244012dc3655
Conor <westcl4@student.op.ac.nz>
2022-10-15 16:03:18 +1300

    Added create compute method skeleton

--- assignment2/assignment.py
+++ assignment2/assignment.py
@@ -6,6 +6,7 @@ import openstack
 
 conn = openstack.connect(cloud_name='openstack', region_name='nz-por-1')
 
+# checking for existing networks
 def getNetwork():
     network = conn.network.find_network(constant.NETWORK)
     return network
@@ -21,21 +22,21 @@ def getRouter():
 def create_network():
     print("Checking network status...")
 
-#define network variables
+    #define network variables
     network = getNetwork()
     subnet = getSubnet()
     router = getRouter()
 
     print(router)
 
-#check for network and create if non existing
+    #check for network and create if non existing
     if (network is None):
         network = conn.network.create_network(
             name=constant.NETWORK
         )
         print("Created network with name " + constant.NETWORK)
 
-# check for subnet and create if non existing
+    # check for subnet and create if non existing
     if (subnet is None):
         subnet = conn.network.create_subnet(
         name=constant.SUBNET,
@@ -46,6 +47,7 @@ def create_network():
         )
         print("Created subnet with name " + constant.SUBNET)
 
+    # Check for router and create if non existing
     if (router is None):   
         try:
             router = conn.network.create_router(
@@ -56,12 +58,20 @@ def create_network():
         router = conn.network.add_interface_to_router(router,subnet_id=subnet.id)
         print(router)
 
+def get_app():
+    app = conn.compute.find_server(constant.APP_NAME)
+    return app
+
+def create_compute():
+    print("Checking compute status...")
+    return
+
 def create():
     ''' Create a set of Openstack resources '''
     print("Create test")
     # print(constant.USER)
     create_network()
-
+    create_compute()
     pass
 
 
--- assignment2/constant.py
+++ assignment2/constant.py
@@ -13,9 +13,12 @@ CIDR = "192.168.50.0/24"
 GATEWAY_IP = "192.168.50.1"
 ROUTERNAME = "westcl4-rtr"
 
-
-
 # compute constants
+IMAGE = "ubuntu-minimal-22.04-x86_64"
+FLAVOUR = "c1.c1r1"
+WEB_NAME = "westcl4-web"
+APP_NAME = "westcl4-app"
+DB_NAME = "westcl4-db"
 
 # run constants
 
