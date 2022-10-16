d3652fa865abc6be5c29e753615857bf207f7bf4
Conor <westcl4@student.op.ac.nz>
2022-10-16 16:43:39 +1300

    Create compute function now fully operational

--- assignment2/assignment.py
+++ assignment2/assignment.py
@@ -1,5 +1,6 @@
 # Adapted from examples on the openstack github https://github.com/openstack/openstacksdk/tree/master/examples/compute
 
+from unicodedata import name
 import constant
 import argparse
 import openstack
@@ -27,8 +28,6 @@ def create_network():
     subnet = getSubnet()
     router = getRouter()
 
-    print(router)
-
     #check for network and create if non existing
     if (network is None):
         network = conn.network.create_network(
@@ -51,25 +50,84 @@ def create_network():
     if (router is None):   
         try:
             router = conn.network.create_router(
-                name= "westcl4-net", external_gateway_info={"network_id": conn.network.find_network("public-net").id})
-            print("Created router with name ")
+                name= "westcl4-net", 
+                external_gateway_info={"network_id": conn.network.find_network("public-net").id})
+            print("Created router with attributes: ")
         except:
             print("Router creation failed")
         router = conn.network.add_interface_to_router(router,subnet_id=subnet.id)
         print(router)
 
+# checking for existing compute
 def get_app():
     app = conn.compute.find_server(constant.APP_NAME)
     return app
 
+def get_web():
+    web = conn.compute.find_server(constant.WEB_NAME)
+    return web
+
+def get_db():
+    db = conn.compute.find_server(constant.DB_NAME)
+    return db
+
 def create_compute():
     print("Checking compute status...")
+    #define compute variables
+    app = get_app()
+    web = get_web()
+    db = get_db()
+
+    print(app)
+
+    #check for app server and create if non existing
+    if (app is None):
+        def create_app():
+            print("Creating app server with attributes: ")
+            app = conn.compute.create_server(
+                name = constant.APP_NAME,
+                image_id = conn.compute.find_image(constant.IMAGE).id,
+                flavor_id = conn.compute.find_flavor(constant.FLAVOUR).id,
+                networks = [{"uuid": conn.network.find_network(constant.NETWORK).id}]
+            )
+            app = conn.compute.wait_for_server(app)
+            print(app)
+        create_app()
+
+    #check for web server and create if non existing
+    if (web is None):
+        def create_web():
+            print("Creating web server with attributes: ")
+            web = conn.compute.create_server(
+                name = constant.WEB_NAME,
+                image_id = conn.compute.find_image(constant.IMAGE).id,
+                flavor_id = conn.compute.find_flavor(constant.FLAVOUR).id,
+                networks = [{"uuid": conn.network.find_network(constant.NETWORK).id}]
+            )
+            web = conn.compute.wait_for_server(web)
+            print(web)
+        create_web()
+    
+    #check for db server and create if non existing
+    if (db is None):
+        def create_db():
+            print("Creating db server with attributes: ")
+            db = conn.compute.create_server(
+                name = constant.DB_NAME,
+                image_id = conn.compute.find_image(constant.IMAGE).id,
+                flavor_id = conn.compute.find_flavor(constant.FLAVOUR).id,
+                networks = [{"uuid": conn.network.find_network(constant.NETWORK).id}]
+            )
+            db = conn.compute.wait_for_server(db)
+            print(db)
+        create_db()
     return
+
+
 
 def create():
     ''' Create a set of Openstack resources '''
-    print("Create test")
-    # print(constant.USER)
+    print("Creating resources...")
     create_network()
     create_compute()
     pass
